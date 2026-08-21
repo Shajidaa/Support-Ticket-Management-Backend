b7e0b21223e47e8c017cd8231b33c911f2fff6e6
Shajidaa <shajidaakterlopa101@gmail.com>
2026-08-21 17:14:13 +0600

    refector(ticket):ticket created successfully

--- src/app/modules/tickets/ticket.model.ts
+++ src/app/modules/tickets/ticket.model.ts
@@ -35,6 +35,7 @@ const ticketSchema = new Schema<ITicket>(
   },
   {
     timestamps: true,
+    versionKey: false,
   },
 );
 
--- src/app/modules/tickets/ticket.service.ts
+++ src/app/modules/tickets/ticket.service.ts
@@ -1,7 +1,17 @@
 import { ITicket } from "./ticket.interface";
+import { Ticket } from "./ticket.model";
 
-const createdTicketDb = async (payload: ITicket, id: string) => {
-  console.log({ payload, id });
+const createdTicketDb = async (payload: ITicket, customerId: string) => {
+  const { title, description, priority, status } = payload;
+
+  const ticket = await Ticket.create({
+    title,
+    description,
+    status,
+    priority,
+    customer: customerId,
+  });
+  return ticket;
 };
 export const ticketService = {
   createdTicketDb,
