3ae14c6ecd1747dd83578069cac6368bb19764bf
Shajidaa <shajidaakterlopa101@gmail.com>
2026-08-29 22:28:15 +0600

    feat(config):update the allowedOrigins name

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -12,5 +12,5 @@ export default {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
-  allowedOrigins: process.env.APP_URL!,
+  allowedOrigins: process.env.NEXT_PUBLIC_API_URL!,
 };
